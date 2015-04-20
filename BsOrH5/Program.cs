92febc531080f99e671399e284b04097be29463e
hanyinglong <han.yinglong@etaostars.com>
2015-04-20 09:49:48 +0800

    添加HTML5和BootStrap的方法

---
+++ BsOrH5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BsOrH5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //备注：学习BootStrap和Html5，
+            //博客：http://aehyok.com/Blog/Detail/5.html
+        }
+    }
+}
