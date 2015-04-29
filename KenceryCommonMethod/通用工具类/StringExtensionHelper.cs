1ed9fe58106db8b480920bd4965e4c596e358792
hanyinglong <han.yinglong@etaostars.com>
2015-04-29 09:53:39 +0800

    正则表达式的添加和修改

--- KenceryCommonMethod/通用工具类/StringExtensionHelper.cs
+++ KenceryCommonMethod/通用工具类/StringExtensionHelper.cs
@@ -6,7 +6,6 @@
 // 创建时间：2015/01/14
 // </copyright>
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -14,13 +13,13 @@ using System.Text.RegularExpressions;
 namespace KenceryCommonMethod
 {
     /// <summary>
-    /// 针对字符串的扩展方法
+    /// 针对字符串的扩展方法(正则表达式)
     /// <auther>
     ///     <name>Kencery</name>
     ///     <date>2015/01/14</date>
     /// </auther>
     /// 修改记录：时间  内容  姓名
-    ///     1.
+    ///     1.2015-04-29   添加和修改正则表达式的验证信息  Kencery
     /// </summary>
     public static class StringExtensionHelper
     {
@@ -32,7 +31,7 @@ namespace KenceryCommonMethod
         /// <returns>如果正则表达式找到匹配项，则为true,否则为false</returns>
         public static bool IsMatch(this string value, string pattern)
         {
-            return value != null && Regex.IsMatch(value, pattern);
+            return value != null && Regex.IsMatch(value, pattern, RegexOptions.IgnoreCase);
         }
 
         /// <summary>
@@ -43,7 +42,7 @@ namespace KenceryCommonMethod
         /// <returns>返回一个对象，包含有关匹配项的信息</returns>
         public static string Match(this string value, string pattern)
         {
-            return value == null ? null : Regex.Match(value, pattern).Value;
+            return value == null ? null : Regex.Match(value, pattern, RegexOptions.IgnoreCase).Value;
         }
 
         /// <summary>
@@ -58,11 +57,88 @@ namespace KenceryCommonMethod
             {
                 return new string[] {};
             }
-            MatchCollection matchCollection = Regex.Matches(value, pattern);
+            MatchCollection matchCollection = Regex.Matches(value, pattern, RegexOptions.IgnoreCase);
             //使用Linq返回集合
             var linqCollection = from Match n in matchCollection
                 select n.Value;
             return linqCollection;
         }
+
+        #region----------------字符串正则表达式验证(Email,Ip,整数，Unicode,Url，身份证号，电话)----------
+
+        /// <summary>
+        /// 使用正则表达式验证是否是电子邮件
+        /// </summary>
+        public static bool IsEmail(this string value)
+        {
+            const string pattern = @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$";
+            return value.IsMatch(pattern);
+        }
+
+        /// <summary>
+        /// 使用正则表达式验证是否是IP地址
+        /// </summary>
+        public static bool IsIpAddress(this string value)
+        {
+            const string pattern =
+                @"^(\d(25[0-5]|2[0-4][0-9]|1?[0-9]?[0-9])\d\.){3}\d(25[0-5]|2[0-4][0-9]|1?[0-9]?[0-9])\d$";
+            return value.IsMatch(pattern);
+        }
+
+        /// <summary>
+        /// 使用正则表达式验证是否整数
+        /// </summary>
+        public static bool IsNumeric(this string value)
+        {
+            const string pattern = @"^\-?[0-9]+$";
+            return value.IsMatch(pattern);
+        }
+
+        /// <summary>
+        /// 使用正则表达式验证是否Unicode字符串
+        /// </summary>
+        public static bool IsUnicode(this string value)
+        {
+            const string pattern = @"^[\u4E00-\u9FA5\uE815-\uFA29]+$";
+            return value.IsMatch(pattern);
+        }
+
+        /// <summary>
+        /// 使用正则表达式验证是否Url字符串
+        /// </summary>
+        public static bool IsUrl(this string value)
+        {
+            const string pattern =
+                @"^(http|https|ftp|rtsp|mms):(\/\/|\\\\)[A-Za-z0-9%\-_@]+\.[A-Za-z0-9%\-_@]+[A-Za-z0-9\.\/=\?%\-&_~`@:\+!;]*$";
+            return value.IsMatch(pattern);
+        }
+
+        /// <summary>
+        /// 使用正则表达式验证是否是身份证，包含以下三种情况：
+        ///     (1):身份证号码为15位数字
+        ///     (2):身份证号码为18位数字
+        ///     (3):身份证号码为17位数字+1个字母
+        /// </summary>
+        public static bool IsIdentityCard(this string value)
+        {
+            const string pattern = @"^(^\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$";
+            return value.IsMatch(pattern);
+        }
+
+        /// <summary>
+        /// 使用正则表达式验证是否是电话号码
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="isRestrict">是否按照严格的格式去验证</param>
+        /// <returns></returns>
+        public static bool IsMobileNumber(this string value, bool isRestrict = false)
+        {
+            string pattern = isRestrict ? @"^[1][3-8]\d{9}$" : @"^[1]\d{10}$";
+            return value.IsMatch(pattern);
+        }
+
+        #endregion
+
+
     }
 }
--- KenceryCommonMethod/通用工具类/ToolsHelper.cs
+++ KenceryCommonMethod/通用工具类/ToolsHelper.cs
@@ -55,7 +55,7 @@ namespace KenceryCommonMethod
         /// </summary>
         /// <param name="value">要分割的字符串</param>
         /// <param name="strSplit">字符串类型的分隔符</param>
-        /// <param name="removeEmptyEntries">是否移除数据中元素为空字符串的项</param>
+        /// <param name="removeEmptyEntries">是否移除数据中元素为空字符串的项，true为移除，false为不移除</param>
         /// <returns>返回分割后的字符串数组</returns>
         public static string[] ConvertStringInfo(this string value, string strSplit = ",",
             bool removeEmptyEntries = false)
@@ -66,79 +66,6 @@ namespace KenceryCommonMethod
 
         #endregion
 
-        #region----------------字符串正则表达式验证(Email,Ip,整数，Unicode,Url，身份证号，电话)----------
 
-        /// <summary>
-        /// 使用正则表达式验证是否是电子邮件
-        /// </summary>
-        public static bool IsEmail(this string value)
-        {
-            const string pattern = @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$";
-            return value.IsMatch(pattern);
-        }
-
-        /// <summary>
-        /// 使用正则表达式验证是否是IP地址
-        /// </summary>
-        public static bool IsIpAddress(this string value)
-        {
-            const string pattern =
-                @"^(\d(25[0-5]|2[0-4][0-9]|1?[0-9]?[0-9])\d\.){3}\d(25[0-5]|2[0-4][0-9]|1?[0-9]?[0-9])\d$";
-            return value.IsMatch(pattern);
-        }
-
-        /// <summary>
-        /// 使用正则表达式验证是否整数
-        /// </summary>
-        public static bool IsNumeric(this string value)
-        {
-            const string pattern = @"^\-?[0-9]+$";
-            return value.IsMatch(pattern);
-        }
-
-        /// <summary>
-        /// 使用正则表达式验证是否Unicode字符串
-        /// </summary>
-        public static bool IsUnicode(this string value)
-        {
-            const string pattern = @"^[\u4E00-\u9FA5\uE815-\uFA29]+$";
-            return value.IsMatch(pattern);
-        }
-
-        /// <summary>
-        /// 使用正则表达式验证是否Url字符串
-        /// </summary>
-        public static bool IsUrl(this string value)
-        {
-            const string pattern =
-                @"^(http|https|ftp|rtsp|mms):(\/\/|\\\\)[A-Za-z0-9%\-_@]+\.[A-Za-z0-9%\-_@]+[A-Za-z0-9\.\/=\?%\-&_~`@:\+!;]*$";
-            return value.IsMatch(pattern);
-        }
-
-        /// <summary>
-        /// 使用正则表达式验证是否是身份证，包含以下三种情况：
-        ///     (1):身份证号码为15位数字
-        ///     (2):身份证号码为18位数字
-        ///     (3):身份证号码为17位数字+1个字母
-        /// </summary>
-        public static bool IsIdentityCard(this string value)
-        {
-            const string pattern = @"^(^\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$";
-            return value.IsMatch(pattern);
-        }
-
-        /// <summary>
-        /// 使用正则表达式验证是否是电话号码
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="isRestrict">是否按照严格的格式去验证</param>
-        /// <returns></returns>
-        public static bool IsMobileNumber(this string value, bool isRestrict = false)
-        {
-            string pattern = isRestrict ? @"^[1][3-8]\d{9}$" : @"^[1]\d{10}$";
-            return value.IsMatch(pattern);
-        }
-
-        #endregion
     }
-}      
+}
