df745dea464afa5d70204ff3adcfcd3d3fb65052
hanyinglong <han.yinglong@etaostars.com>
2015-07-10 12:01:04 +0800

    Quartz.NET作业调度框架学习建立

---
+++ Quartz.NET/Program.cs
@@ -0,0 +1,16 @@
+﻿// 源文件头信息：
+// <copyright file="Program.cs">
+// Copyright(c)2014-2034 Kencery.All rights reserved.
+// 个人博客：http://www.cnblogs.com/hanyinglong
+// 创建人：韩迎龙(kencery)
+// 创建时间：2015-7-10
+// </copyright>
+namespace Quartz.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
