f375e9ff23faa1e074f90ed611b72bd585616ea3
hanyinglong <han.yinglong@etaostars.com>
2015-08-07 10:32:29 +0800

    特性整理信息

---
+++ KenceryCommonMethod/AuthorizeAttribute/LoginAttribute.cs
@@ -0,0 +1,28 @@
+﻿// 源文件头信息：
+// <copyright file="LoginAttribute.cs">
+// Copyright(c)2014-2034 Kencery.All rights reserved.
+// 个人博客：http://www.cnblogs.com/hanyinglong
+// 创建人：韩迎龙(kencery)
+// 创建时间：2015-8-7
+// </copyright>
+
+using System;
+using System.Web.Mvc;
+
+namespace KenceryCommonMethod
+{
+    /// <summary>
+    /// <auther>
+    ///     <name>Kencery</name>
+    ///     <date>2015-8-7</date>
+    /// </auther>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = true, AllowMultiple = true)]
+    public class LoginAttribute : FilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            throw new global::System.NotImplementedException();
+        }
+    }
+}
--- KenceryCommonMethod/Json对象转换/JsonNetHelper.cs
+++ KenceryCommonMethod/Json对象转换/JsonNetHelper.cs
@@ -3,7 +3,7 @@
 // Copyright(c)2014-2034 Kencery.All rights reserved.
 // 个人博客：http://www.cnblogs.com/hanyinglong
 // 创建人：韩迎龙(kencery)
-// 创建时间：2014/7/7 09:30
+// 创建时间：2014-7-7
 // </copyright>
 
 using System;
