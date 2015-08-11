3f83e03a57c65754b2a584b8cb11331c4ddd2403
hanyinglong <han.yinglong@etaostars.com>
2015-08-11 16:15:40 +0800

    HttpRuntimeCache缓存的创建

---
+++ KenceryCommonMethod/缓存/HttpRuntimeCache.cs
@@ -0,0 +1,25 @@
+﻿// 源文件头信息：
+// <copyright file="HttpRuntimeCache.cs">
+// Copyright(c)2014-2034 Kencery.All rights reserved.
+// 个人博客：http://www.cnblogs.com/hanyinglong
+// 创建人：韩迎龙(kencery)
+// 创建时间：2015-8-11
+// </copyright>
+namespace KenceryCommonMethod
+{
+    /// <summary>
+    /// HttpRuntime Cache读取设置缓存信息封装
+    /// <auther>
+    ///     <name>Kencery</name>
+    ///     <date>2015-8-11</date>
+    /// </auther>
+    /// 使用描述：
+    /// 
+    /// </summary>
+    public class HttpRuntimeCache
+    {
+
+
+
+    }
+}
