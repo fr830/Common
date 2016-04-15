6afff571438401dcd5badbeea910fc7b25840395
hanyinglong <han.yinglong@etaostars.com>
2016-04-15 16:50:40 +0800

    文件操作合并-加密解密、是否存在、查找文件内部的内容

--- common_method/文件操作File/FileBaseHelper.cs
+++ common_method/文件操作File/FileBaseHelper.cs
@@ -8,6 +8,8 @@
 
 using System;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace KenceryCommonMethod
 {
@@ -17,6 +19,7 @@ namespace KenceryCommonMethod
     ///     <name>Kencery</name>
     ///     <date>2015/04/30</date>
     /// </auther>
+    /// 修改：合并文件加密解密共用类 2016-4-15  Kencery
     /// </summary>
     public static class FileBaseHelper
     {
@@ -73,7 +76,7 @@ namespace KenceryCommonMethod
                     do
                     {
                         string readLine = streamReader.ReadLine(); //读取的信息
-                        if (string.IsNullOrEmpty(readLine))
+                        if (String.IsNullOrEmpty(readLine))
                         {
                             break;
                         }
@@ -103,5 +106,71 @@ namespace KenceryCommonMethod
             return isTrue;
         }
 
+        public const string FileKey = "ihlih*0037JOHT*)(PIJY*(()JI^)IO%"; //加密密钥
+
+        /// <summary>
+        /// 对文件进行加密
+        /// 调用:FileEncryptHelper.FileEncryptInfo(Server.MapPath("~" +路径), Server.MapPath("~" +路径), FileHelper.FileEncrityKey)
+        /// </summary>
+        /// <param name="fileOriginalPath">需要加密的文件路径</param>
+        /// <param name="fileFinshPath">加密完成后存放的文件路径</param>
+        /// <param name="fileKey">文件密钥</param>
+        public static void FileEncryptInfo(string fileOriginalPath, string fileFinshPath, string fileKey)
+        {
+            //分组加密算法的实现
+            using (var fileStream = new FileStream(fileOriginalPath, FileMode.Open))
+            {
+                var buffer = new Byte[fileStream.Length];
+                fileStream.Read(buffer, 0, buffer.Length); //得到需要加密的字节数组
+                //设置密钥，密钥向量，两个一样，都是16个字节byte
+                var rDel = new RijndaelManaged
+                {
+                    Key = Encoding.UTF8.GetBytes(fileKey),
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.PKCS7
+                };
+                ICryptoTransform cryptoTransform = rDel.CreateEncryptor();
+                byte[] cipherBytes = cryptoTransform.TransformFinalBlock(buffer, 0, buffer.Length);
+                using (var fileSEncrypt = new FileStream(fileFinshPath, FileMode.Create, FileAccess.Write))
+                {
+                    fileSEncrypt.Write(cipherBytes, 0, cipherBytes.Length);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 对文件进行解密
+        /// 调用:FileEncryptHelper.FileDecryptInfo(Server.MapPath("~" +路径), Server.MapPath("~" +路径), FileHelper.FileEncrityKey)
+        /// </summary>
+        /// <param name="fileFinshPath">传递需要解密的文件路径</param>
+        /// <param name="fileOriginalPath">解密后文件存放的路径</param>
+        /// <param name="fileKey">密钥</param>
+        public static void FileDecryptInfo(string fileFinshPath, string fileOriginalPath, string fileKey)
+        {
+            using (var fileStreamIn = new FileStream(fileFinshPath, FileMode.Open, FileAccess.Read))
+            {
+                using (var fileStreamOut = new FileStream(fileOriginalPath, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    var rDel = new RijndaelManaged
+                    {
+                        Key = Encoding.UTF8.GetBytes(fileKey),
+                        Mode = CipherMode.ECB,
+                        Padding = PaddingMode.PKCS7
+                    };
+                    using (var cryptoStream = new CryptoStream(fileStreamOut, rDel.CreateDecryptor(),
+                        CryptoStreamMode.Write))
+                    {
+                        var bufferLen = 4096;
+                        var buffer = new byte[bufferLen];
+                        int bytesRead;
+                        do
+                        {
+                            bytesRead = fileStreamIn.Read(buffer, 0, bufferLen);
+                            cryptoStream.Write(buffer, 0, bytesRead);
+                        } while (bytesRead != 0);
+                    }
+                }
+            }
+        }
     }
 }
