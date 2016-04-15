cfa3012f60d1ab3171e498cb94b417910f0fde5b
hanyinglong <han.yinglong@etaostars.com>
2016-04-15 16:30:05 +0800

    对象转换器-Linq

--- common_method/对象转化器/ObjectConvertHelper.cs
+++ common_method/对象转化器/ObjectConvertHelper.cs
@@ -20,8 +20,6 @@ namespace KenceryCommonMethod
     ///     <name>Kencery</name>
     ///     <date>2014/12/11</date>
     /// </auther>
-    /// 修改记录：时间  内容  姓名
-    ///     1.
     /// </summary>
     public static class ObjectConvertHelper
     {
