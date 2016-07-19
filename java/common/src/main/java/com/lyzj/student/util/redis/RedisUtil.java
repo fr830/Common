3b33495976f8fcf69ec5839357d90d2ffd79cd5a
hanyinglong <han.yinglong@etaostars.com>
2016-07-19 16:11:15 +0800

    对redis的封装以及测试完成

--- java/common/src/main/java/com/lyzj/student/util/redis/RedisUtil.java
+++ java/common/src/main/java/com/lyzj/student/util/redis/RedisUtil.java
@@ -5,11 +5,12 @@ import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 封装Redis的通用方法(明天搞定日志)
+ * 封装Redis的通用方法
  * Created by kencery on 2016/7/18.
  */
 @Component
@@ -66,7 +67,6 @@ public class RedisUtil {
         }
     }
 
-
     /**
      * 根据键设置过期时间
      *
@@ -113,6 +113,33 @@ public class RedisUtil {
         }
     }
 
+    /**
+     * 设置哈希,不设置过期时间
+     *
+     * @param key     键
+     * @param hashMap 哈希值
+     */
+    public void setHash(String key, HashMap hashMap) {
+        setHash(key, hashMap, -1);
+    }
+
+    /**
+     * 设置哈希,设置过期时间
+     *
+     * @param key        键
+     * @param hashMap    值
+     * @param expireTime 过期时间
+     */
+    public void setHash(String key, HashMap hashMap, long expireTime) {
+        try {
+            redisTemplate.opsForHash().putAll(key, hashMap);
+            if (expireTime > 0) {
+                setExpire(key, expireTime, TimeUnit.MINUTES);
+            }
+        } catch (Exception e) {
+            //写入日志
+        }
+    }
 
     /**
      * 根据键删除内容
@@ -149,6 +176,37 @@ public class RedisUtil {
     }
 
     /**
+     * 根据Key判断键是否存在
+     *
+     * @param key 键
+     * @return
+     */
+    public boolean exists(final String key) {
+        try {
+            return (boolean) redisTemplate.execute((RedisCallback) connection -> connection.exists(key.getBytes()));
+        } catch (Exception e) {
+            //写入日志
+            return false;
+        }
+    }
+
+    /**
+     * 判断哈希Key是否存在
+     *
+     * @param key     键
+     * @param hashKey 哈希键
+     * @return
+     */
+    public boolean hashKeyExists(String key, Object hashKey) {
+        try {
+            return redisTemplate.opsForHash().hasKey(key, hashKey);
+        } catch (Exception e) {
+            //写入日志
+            return false;
+        }
+    }
+
+    /**
      * 根据key读取Value
      *
      * @param key 键
@@ -164,17 +222,33 @@ public class RedisUtil {
     }
 
     /**
-     * 根据Key判断键是否存在
+     * 根据key查询出来全部的集合信息
      *
-     * @param key 键
+     * @param key
      * @return
      */
-    public boolean exists(final String key) {
+    public List range(String key) {
         try {
-            return (boolean) redisTemplate.execute((RedisCallback) connection -> connection.exists(key.getBytes()));
+            return redisTemplate.opsForList().range(key, 0, -1);
         } catch (Exception e) {
             //写入日志
-            return false;
+            return null;
+        }
+    }
+
+    /**
+     * 获取Hash值
+     *
+     * @param key     键
+     * @param hashKey 哈希键
+     * @return
+     */
+    public Object getHashValue(String key, Object hashKey) {
+        try {
+            return redisTemplate.opsForHash().get(key, hashKey);
+        } catch (Exception e) {
+            //写入日志
+            return null;
         }
     }
 }
--- java/common/src/test/java/com/lyzj/student/RedisTest.java
+++ java/common/src/test/java/com/lyzj/student/RedisTest.java
@@ -8,6 +8,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -15,78 +21,114 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by kencery on 2016/7/18.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:/spring.xml", "classpath:/spring-*.xml"})
+@WebAppConfiguration
+@ContextConfiguration({"classpath:/spring.xml", "classpath:/spring-redis.xml"})
 public class RedisTest {
 
     @Autowired
     RedisUtil redisUtil;
 
-    @Test
-    public  void  setTest() {
-        redisUtil.set(RedisCacheKeys.testKey, "test", 1);
-        Object o = redisUtil.getValue(RedisCacheKeys.testKey);
-        Assert.assertEquals(0, "test");
-    }
-
-/*
+    /**
+     * 测试存储键值对，不设置过期时间
+     */
     @Test
     public void setTest() {
-        redisUtil.set(RedisCacheKeys.testKey, "test", 1);
+        redisUtil.set(RedisCacheKeys.testKey, "Kencery");
         Object o = redisUtil.getValue(RedisCacheKeys.testKey);
-        Assert.assertEquals(o, "test");
+        Assert.assertEquals(o, "Kencery");
         redisUtil.del(RedisCacheKeys.testKey);
     }
 
+    /**
+     * 测试存储键值对，设置过期时间
+     */
     @Test
-    public void existsTest() {
-        System.out.println(redisUtil.exists("test"));
+    public void setTest1() {
+        redisUtil.set(RedisCacheKeys.testKey, "Kencery", 1);
+        Object o = redisUtil.getValue(RedisCacheKeys.testKey);
+        Assert.assertEquals(o, "Kencery");
     }
 
+    /**
+     * 根据键设置过期时间
+     */
     @Test
-    public void delsTest() {
-        redisUtil.set(RedisCacheKeys.testKey, "test", 1);
-        redisUtil.set("com.etao.cashier.delstest", "test", 1);
-
-        System.out.println("删除key:" + redisUtil.del(RedisCacheKeys.testKey, "com.etao.cashier.delstest"));
+    public void setExpireTest() {
+        redisUtil.setExpire("newkey", 1, TimeUnit.MINUTES);
     }
 
+    /**
+     * 存入列表,不设置过期时间
+     */
     @Test
-    public void saveListTest() {
+    public void rightPushAllTest() {
         List<String> list = new ArrayList<>();
-        list.add("mike");
-        list.add("张三");
+        list.add("zhangsan");
+        list.add("lisi");
+        redisUtil.rightPushAll(RedisCacheKeys.testKey, list);
 
-        redisUtil.set(RedisCacheKeys.testKey, list, 1);
-
-        List result = (List) redisUtil.getValue(RedisCacheKeys.testKey);
+        List result = redisUtil.range(RedisCacheKeys.testKey);
         System.out.println(result);
         redisUtil.del(RedisCacheKeys.testKey);
     }
 
+    /**
+     * 存入列表,设置过期时间
+     */
     @Test
-    public void rightPushAllTest() {
+    public void rightPushAllTest1() {
         List<String> list = new ArrayList<>();
-        list.add("mike");
-        list.add("张三");
+        list.add("chengyuanping");
+        list.add("hanyinglong");
+        redisUtil.rightPushAll(RedisCacheKeys.testKey, list, 1);
 
-        System.out.println(redisUtil.rightPushAll(RedisCacheKeys.testKey, list));
+        List result = (List) redisUtil.range(RedisCacheKeys.testKey);
+        System.out.println(result);
+    }
 
-        System.out.println(redisUtil.range(RedisCacheKeys.testKey));
+    /**
+     * 存入哈希，不设置过期时间
+     */
+    @Test
+    public void setHashTest() {
+        HashMap hashtable = new HashMap();
+        hashtable.put(1, "Hello");
+        hashtable.put(2, "World");
+        hashtable.put(3, "Redis");
+        redisUtil.setHash(RedisCacheKeys.testKey, hashtable);
+        System.out.println("哈希键为3的值:" + redisUtil.getHashValue(RedisCacheKeys.testKey, 3));
 
         redisUtil.del(RedisCacheKeys.testKey);
     }
 
+    /**
+     * 存入哈希，设置过期时间
+     */
     @Test
-    public void hashTest() {
+    public void setHashTest1() {
         HashMap hashtable = new HashMap();
-        hashtable.put(1, "你好");
-        hashtable.put(2, "错误");
-        hashtable.put(3, "hello world");
-        String key = RedisCacheKeys.testKey + ".hash";
-        redisUtil.setHash(key, hashtable);
+        hashtable.put(1, "Hello");
+        hashtable.put(2, "World");
+        hashtable.put(3, "Redis");
+        redisUtil.setHash(RedisCacheKeys.testKey, hashtable, 1);
+        System.out.println("哈希键为1的值:" + redisUtil.getHashValue(RedisCacheKeys.testKey, 1));
+    }
 
-        System.out.println("哈希键为3的值:" + redisUtil.getHashValue(key, 3));
+    /**
+     * 根据key验证是否存在
+     */
+    @Test
+    public void existsTest() {
+        boolean isBool = redisUtil.exists("mykey1");
+        System.out.println(isBool);
+    }
 
-        redisUtil.del(key);
-    }*/
+    /**
+     * 根据多个变量删除redis中的内容
+     */
+    @Test
+    public void delTest() {
+        Object o = redisUtil.del("mykey1", "t2", "t1");
+        System.out.println(o);
+    }
 }
