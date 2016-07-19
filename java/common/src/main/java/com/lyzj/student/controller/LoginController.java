b6510af5639eec9e121c51f01cd5024eefd4cb5b
hanyinglong <han.yinglong@etaostars.com>
2016-07-19 11:53:12 +0800

    redis本机测试完成

--- java/common/src/main/java/com/lyzj/student/controller/LoginController.java
+++ java/common/src/main/java/com/lyzj/student/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.lyzj.student.controller;
 
+import com.lyzj.student.util.redis.RedisCacheKeys;
+import com.lyzj.student.util.redis.RedisUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/login")
 @Api(tags = "Login_Api",description = "登录业务逻辑接口调用")
 public class LoginController {
+
+    @Autowired
+    RedisUtil redisUtil;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     @ResponseBody
@@ -27,7 +33,8 @@ public class LoginController {
     public String edit(@RequestParam(value = "", name = "署名", required = true) String signature,
                        @RequestParam(value = "", name = "时间戳", required = false) String timestamp,
                        @RequestBody(required = false) String xmlStr) {
-        return "edit";
+        redisUtil.set(RedisCacheKeys.testKey, "test", 1);
+        Object o = redisUtil.getValue(RedisCacheKeys.testKey);
+        return o.toString();
     }
-
 }
